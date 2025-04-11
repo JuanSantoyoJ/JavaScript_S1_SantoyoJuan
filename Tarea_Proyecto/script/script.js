c2495ddee6cbd74d2667007dbeae24e35b646fea
JuanSantoyoJ <juanda.c0512@gmail.com>
2025-04-11 08:40:51 -0500

    feat: :fire:

--- Tarea_Proyecto/script/script.js
+++ Tarea_Proyecto/script/script.js
@@ -3,4 +3,19 @@ import * as menus from './menus.js';
 import * as trainers from './trainers.js';
 
 menus.menuInicio();
-let usuario = prompt("Ingrese el tipo de usuario: \n1. Camper\n2. Trainer\n3. Coordinador");
+let usuario = prompt("Ingrese el tipo de usuario:");
+if (usuario == 1) {
+    menus.menuCamper();
+    let opcCamper = prompt("Seleccione una opcion:");
+    if (opcCamper == 1) {
+    }
+} else if (usuario == 2) {
+    menus.menuTrainer();
+} else if (usuario == 3) {
+    menus.menuCoordinador();
+    let opcCoordinador = prompt("Seleccione una opcion:");
+    if (opcCoordinador == 4) {
+        let camperData = campers.agregarCamper();
+        listaCampers.push(camperData);
+    }
+}
