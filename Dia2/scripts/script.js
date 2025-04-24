f3a6a4b3c16b1fd34f06bbc2ff72df0d3ace8d61
JuanSantoyoJ <juanda.c0512@gmail.com>
2025-04-24 08:41:59 -0500

    feat: :skull: todo mal

--- Dia2/scripts/script.js
+++ Dia2/scripts/script.js
@@ -1,3 +1,10 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonGif = document.querySelector('.pokemon__gif');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
 const buscarPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
@@ -5,3 +12,15 @@ const buscarPokemon = async (pokemon) => {
 
     return data;
 }
+const renderPokemon = async (pokemon) => {
+    const data = await buscarPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonGif.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+}
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value);
+    input.value = '';
+});
